67191daa7054e7a4a51f9d58fdef322b86472936
Alec Arteaga <arteagajacob@gmail.com>
2022-09-18 13:32:27 -0700

    first commit

---
+++ great_number_game.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, session, request
+import random
+
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'num' not in session:
+        session['num'] = random.randint(1,100)
+    else:
+        session['count'] += 1
+    return render_template('index.html')
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
